20960ae410d2b153bfb6c95639370e273e9df181
quanvo3 <votrandongquan@gmail.com>
2022-04-22 19:16:25 +0700

    Login and signup

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Header from './pages/user/Layouts/Header/Header.js'
+import SignInSide from './pages/user/Login/index.js'
+import SignUp from './pages/user/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> <SignInSide/> <SignUp/>
+      */}
+     <SignInSide/>
     </div>
   );
 }
