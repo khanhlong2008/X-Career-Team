707f1caf463e7f45afa4bf2b99649f0b9f47e19d
nguyenquy42 <nguyenquy26012014@gmail.com>
2022-04-26 21:38:09 +0700

    add data menu

--- src/pages/admin/Dashboard/Dashboard.js
+++ src/pages/admin/Dashboard/Dashboard.js
@@ -7,71 +7,65 @@ import "../admin.main.css";
 
 const { Header, Content, Sider } = Layout;
 
-const items1 = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
-
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-  const key = String(index + 1);
-  return {
-    key: `sub${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`,
-      };
-    }),
-  };
+const items2 = [{ icon: UserOutlined, titleName: 'Bài Viết', child: [{ subKey: '1', sublabel: 'Chi tiết' }, { subKey: '2', sublabel: 'thu 2' }] }, { icon: UserOutlined, titleName: 'Sản Phẩm', child: [{ subKey: '1', sublabel: 'Chi tiết' }, { subKey: '2', sublabel: 'thu 2' }] }, { icon: LaptopOutlined, titleName: 'Liên Hệ', child: [{ subKey: '1', sublabel: 'Chi tiết' }, { subKey: '2', sublabel: 'thu 2' }] }, { icon: NotificationOutlined, titleName: 'Cài Đặt', child: [{ subKey: '1', sublabel: 'Chi tiết' }, { subKey: '2', sublabel: 'thu 2' }] }].map((data, index) => {
+    const key = data.titleName;
+    return {
+        key: `sub${key}`,
+        icon: React.createElement(data.icon),
+        label: `${key}`,
+        children: data.child.map((data, j) => {
+            return {
+                key: data.subKey,
+                label: `${data.sublabel}`,
+            };
+        }),
+    };
 });
 
 const Dashboard = () => {
-  return (
-    <Layout style={{
-      minHeight: '100vh',
-    }}>
-      <Header className="header">
-        <div className="logo">
-        Team ProjectX
-        </div>
-      </Header>
-      <Layout>
-        <Sider width={200} className="site-layout-background">
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{
-              height: '100%',
-              borderRight: 0,
-            }}
-            items={items2}
-            theme="dark"
-          />
-        </Sider>
-        <Layout
-          style={{
-            padding: '20px',
-          }}
-        >
-          <Content
-            theme="dark"
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-            }}
-          >
-            Content
-          </Content>
+    return (
+        <Layout style={{
+            minHeight: '100vh',
+        }}>
+            <Header className="header">
+                <div className="logo">
+                    Team ProjectX
+                </div>
+            </Header>
+            <Layout>
+                <Sider width={200} className="site-layout-background">
+                    <Menu
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        defaultOpenKeys={['sub1']}
+                        style={{
+                            height: '100%',
+                            borderRight: 0,
+                        }}
+                        items={items2}
+                        theme="dark"
+                    />
+                </Sider>
+                <Layout
+                    style={{
+                        padding: '20px',
+                    }}
+                >
+                    <Content
+                        theme="dark"
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            minHeight: 280,
+                        }}
+                    >
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
         </Layout>
-      </Layout>
-    </Layout>
-  );
+    );
 };
 
 export default Dashboard;
