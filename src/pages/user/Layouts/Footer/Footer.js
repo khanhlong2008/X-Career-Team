a6d1e94306e8ccaa6b3b2879627440902faa21ed
quanvo3 <votrandongquan@gmail.com>
2022-06-12 10:31:06 +0700

    footer added

--- src/pages/user/Layouts/Footer/Footer.js
+++ src/pages/user/Layouts/Footer/Footer.js
@@ -3,23 +3,59 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './Footer.css';
 function Footer(props) {
     return (
-        <div className='d-flex justify-content-center align-items-center flex-column wrapper mt-5'>
-            <div className="aboutUs d-flex justify-content-center align-items-center col-10">
-                    <div className="leftSide d-flex col-6 justify-content-center align-items-center">
-                        <b>Logo</b>
-                        <p>Chỗ này để logo</p>
-                    </div>
-                    <div className="rightSide col-6 d-flex flex-column justify-content-center align-items-center">
-                        <div>add names</div>
-                        <div>add names</div>
-                        <div>add names</div>
-                        <div>add names</div>
-                    </div>
+       
+        <footer class="site-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 col-md-6">
+                <h6>About</h6>
+                <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+              </div>
+    
+              <div class="col-xs-6 col-md-3">
+                <h6>Categories</h6>
+                <ul class="footer-links">
+                  <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+                  <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+                  <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+                  <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+                  <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+                  <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+                </ul>
+              </div>
+    
+              <div class="col-xs-6 col-md-3">
+                <h6>Quick Links</h6>
+                <ul class="footer-links">
+                  <li><a href="http://scanfcode.com/about/">About Us</a></li>
+                  <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+                  <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+                  <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+                  <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+                </ul>
+              </div>
             </div>
-            <div className="socialMedia justify-content-center align-items-center col-10">
-                <div>add social media symbol here</div>
+            <hr/>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-sm-6 col-xs-12">
+                <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+             <a href="#">Scanfcode</a>.
+                </p>
+              </div>
+    
+              <div class="col-md-4 col-sm-6 col-xs-12">
+                <ul class="social-icons">
+                  <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                  <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                  <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+                  <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+                </ul>
+              </div>
             </div>
-        </div>
+          </div>
+    </footer>
     );
 }
 
--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import Footer from "./Layouts/Footer/Footer";
 import Header from "./Layouts/Header/Header";
-
+import './index.css';
 const User = ({ children }) => {
   //render
 
